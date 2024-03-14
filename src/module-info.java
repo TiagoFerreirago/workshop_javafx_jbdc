009bedee2a2a81928cc24c8f670e1034386b1b2e
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-03-14 12:43:57 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module workshop_javafx {
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens model.entities to javafx.base;
